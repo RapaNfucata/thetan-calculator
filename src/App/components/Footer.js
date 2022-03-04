3c981862536eebb9e39acafeced26ac92956d79a
RapaNfucata <100123330+RapaNfucata@users.noreply.github.com>
2022-03-04 09:41:45 +0100

    update v2.1.6
    update a

--- src/App/components/Footer.js
+++ src/App/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   const [titleButton, setTitle] = useState(
-    "Offri un caffè ai miei Thetan Character"
+    "Offri un caffè ai miei Thetan Heroes"
   );
   const address = useRef();
   function copyToClipboard(text) {
@@ -18,7 +18,7 @@ function Footer() {
     copyToClipboard("0x2669a95Ce4301515c1ED0eF75De6F398CBCDc37B");
     setTitle("Indirizzo BSC copiato negli appunti");
     const timer = setTimeout(() => {
-      setTitle("Offri un caffè ai miei Thetan Character");
+      setTitle("Offri un caffè ai miei Thetan Heroes");
       clearTimeout(timer);
     }, 1000);
   }
@@ -31,7 +31,7 @@ function Footer() {
       <div className="footerText">
         Questo è un servizio totalmente gratuito, senza ADs, pubblicità o altro!
         Se avete voglia di supportare il mio lavoro potete offrire un caffè ai
-        miei Thetan Character
+        miei Thetan Heroes
       </div>
       <button className="footerButton" onClick={() => getBSCAddress()}>
         {titleButton}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/thetan-calculator">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
